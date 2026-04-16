782d5013af3b909e60c2a5a91c40258b7dd099e9
lovemyself72026 <lovemyself72026@gmail.com>
2026-04-16 08:02:34 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'love-myself-v3';
+const CACHE = 'love-myself-v4';
 const IMG_CACHE = 'love-myself-images-v1';
 
 const STATIC_FILES = [
@@ -26,6 +26,26 @@ self.addEventListener('activate', e => {
   );
   self.clients.claim();
 });
+
+// Keys that should be synced to Firebase
+const SYNC_KEYS = [
+  'all_planner_data','my_wins','saved_affirmations','vent_entries','dimension_items',
+  'whoi_history','whoi_woh_history','my_books','habit_tracker','cali_log',
+  'wia2','wia2aff','wia2q','wia2wq',
+  'hlb','hlr',
+  'ass_checkin','ass_checkin_log','ass_log','ass_entries_asking','ass_entries_giving',
+  'bel_foundation','bel_evidence','bel_failure','belief_beliefs','bel_checkin_log',
+  'balance_data','that_girl_data','vision_items','ritual_state','user_rituals',
+  'bc_habits_data','face_habits_data','daily_data','rfm_data','jesus_data',
+  'my_routine_data','routine_todos','routine_notes','routine_photos','sched_photos',
+  'infp_data','friends_data'
+];
+
+const SYNC_PREFIXES = [
+  'ass_entries_','ass_notes_',
+  'jesus_','bc_','rfm_',
+  'rhythm_day_','rhythm_month_','rhythm_year_'
+];
 
 // Fetch handler
 self.addEventListener('fetch', e => {
